aef3103e33a6664afdc25c349dc329e85b66206d
Bit128DS <theodledropbox@gmail.com>
2023-01-23 14:20:39 +0100

    working cache

--- update.py
+++ update.py
@@ -6,32 +6,36 @@ import os
 
 prefix = "https://pub.drednot.io/"
 mid = "/econ/"
+cachedir = "cache"
 
 dumpTypeExtensions = {
     "summary":".json",
-    ""
+    "log":".json.gz",
+    "ships":".json.gz",
 }
 
 def mkdir(dir):
     if not os.path.exists(dir):
         os.mkdir(dir)
 
-mkdir("files")
+mkdir(cachedir)
 
-mkdir("files/prod")
-mkdir("files/test")
+mkdir(cachedir + "/prod")
+mkdir(cachedir + "/test")
 
-mkdir("files/prod/summary")
-mkdir("files/prod/log")
-mkdir("files/prod/ships")
+mkdir(cachedir + "/prod/summary")
+mkdir(cachedir + "/prod/log")
+mkdir(cachedir + "/prod/ships")
 
-mkdir("files/test/summary")
-mkdir("files/test/log")
-mkdir("files/test/ships")
+mkdir(cachedir + "/test/summary")
+mkdir(cachedir + "/test/log")
+mkdir(cachedir + "/test/ships")
 
 def saveLinkToFile(link, filepath):
     text = requests.get(link).text
-    with open(filepath, "w") as f:
+    if link[:-3] == ".gz":
+        text = gzip.decompress(text)
+    with open(filepath, "w", encoding="utf-8") as f:
         f.write(text)
 
 def updateDump(
@@ -41,16 +45,18 @@ def updateDump(
     enddate:datetime.date=datetime.date.today()):
     saveLinkToFile(
         prefix + instance + mid + "item_schema.json",
-        os.path.join("files", instance, "item_schema.json"))
+        os.path.join(cachedir, instance, "item_schema.json"))
     for dumpType in dumpTypes:
         date = startdate
         date -= datetime.timedelta(days=1)
         while date < enddate:
-            print("Getting data: {} days left".format(enddate-date).days)
+            print("Getting data {} : {} days left".format(dumpType, (enddate-date).days))
             date += datetime.timedelta(days=1)
             datestr = str(date.year)+"_"+str(date.month)+"_"+str(date.day)
-            filepath = os.path.join("files",instance,dumpType,datestr+".json")
+            filepath = os.path.join(cachedir,instance,dumpType,datestr+".json")
             if not os.path.exists(filepath):
                 saveLinkToFile(
-                    prefix+instance+mid+datestr+"/"+dumpType+dumpTypeExtensions[dumpType]
-                )
+                    prefix+instance+mid+datestr+"/"+dumpType+dumpTypeExtensions[dumpType],
+                    filepath)
+if __name__ == "__main__":
+    updateDump()
